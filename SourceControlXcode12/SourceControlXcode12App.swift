9557a98862564e44848e4dc68f89de16e15976fa
Mohammed <mohammed.alsalhi@outlook.com>
2023-08-17 13:12:16 +0400

    Initial Commit

---
+++ SourceControlXcode12/SourceControlXcode12App.swift
@@ -0,0 +1,17 @@
+//
+//  SourceControlXcode12App.swift
+//  SourceControlXcode12
+//
+//  Created by Mohammed salim Al Salhi on 17/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct SourceControlXcode12App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
